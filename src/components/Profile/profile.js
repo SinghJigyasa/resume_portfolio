9b6ce2fcea697f9f6ee608e22b16324958ff67a4
SinghJigyasa <jig86026@gmail.com>
2025-04-30 01:06:05 +0530

    parcel image issues fixes

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,24 +1,26 @@
 import { Button, Typography } from "@mui/material";
 import React from "react";
 import "./profile.css";
-import imageIcon from "../../assets/images/css-3.png";
+const profileIcon = new URL("../../assets/profileIcon.jpeg", import.meta.url)
+  .href;
+
 const Profile = () => {
-	return (
-		<div className="profile_container">
-			<div className="profile_header">
-				<Typography className="name">Jigyasa Singh</Typography>
-				<Typography className="title">Software Engineer</Typography>
-			</div>
-			<div className="profile_image">
-				<img src={''} alt=" profile Icon" width={'100px'} />
-			</div>
-			<div className="profile_information">
-				Add TileLine
-				<br />
-				<Button>Download</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="profile_container">
+      <div className="profile_header">
+        <Typography className="name">Jigyasa Singh</Typography>
+        <Typography className="title">Software Engineer</Typography>
+      </div>
+      <div className="profile_image">
+        <img src={profileIcon} alt=" profile Icon" width={"100px"} />
+      </div>
+      <div className="profile_information">
+        Add TileLine
+        <br />
+        <Button>Download</Button>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
