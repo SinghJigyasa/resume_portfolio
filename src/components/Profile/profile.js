d2f8ce3523539648285f5816e58b1da181a9d13b
SinghJigyasa <jigyasa.singh@highpeaksw.com>
2025-04-30 01:50:39 +0530

    profile timeline in process

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -1,26 +1,27 @@
 import { Button, Typography } from "@mui/material";
 import React from "react";
 import "./profile.css";
+import CustomTimeLine from "../Timeline/timeLine";
 const profileIcon = new URL("../../assets/profileIcon.jpeg", import.meta.url)
   .href;
 
 const Profile = () => {
   return (
-    <div className="profile_container">
-      <div className="profile_header">
-        <Typography className="name">Jigyasa Singh</Typography>
-        <Typography className="title">Software Engineer</Typography>
-      </div>
-      <div className="profile_image">
-        <img src={profileIcon} alt=" profile Icon" width={"100px"} />
-      </div>
-      <div className="profile_information">
-        Add TileLine
-        <br />
-        <Button>Download</Button>
-      </div>
-    </div>
-  );
+		<div className="profile_container">
+			<div className="profile_header">
+				<Typography className="name">Jigyasa Singh</Typography>
+				<Typography className="title">Software Engineer</Typography>
+			</div>
+			<div className="profile_image">
+				<img src={profileIcon} alt=" profile Icon" />
+			</div>
+			<div className="profile_information">
+				<CustomTimeLine />
+				<br />
+				<Button>Download</Button>
+			</div>
+		</div>
+	);
 };
 
 export default Profile;
---
+++ src/components/Timeline/timeLine.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const CustomTimeLine = () => {
+  return (
+    <div>
+      This is time line component
+    </div>
+  )
+}
+
+export default CustomTimeLine
