8b2fe40d757f0822a98f8aefd614bf19c4ba62c1
SinghJigyasa <jig86026@gmail.com>
2025-05-04 23:17:26 +0530

    profile style update

--- src/components/Timeline/timeLine.js
+++ src/components/Timeline/timeLine.js
@@ -1,11 +1,30 @@
 import React from 'react'
-
+import Timeline from "@mui/lab/Timeline";
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineSeparator from "@mui/lab/TimelineSeparator";
+import TimelineConnector from "@mui/lab/TimelineConnector";
+import TimelineContent from "@mui/lab/TimelineContent";
+import TimelineDot from "@mui/lab/TimelineDot";
 const CustomTimeLine = () => {
   return (
-    <div>
-      This is time line component
-    </div>
-  )
+    <Timeline>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          <TimelineConnector />
+        </TimelineSeparator>
+        <TimelineContent>Eat</TimelineContent>
+      </TimelineItem>
+      <TimelineItem>
+        <TimelineSeparator>
+          <TimelineDot />
+          {/* <TimelineConnector /> */}
+        </TimelineSeparator>
+        <TimelineContent>Code</TimelineContent>
+      </TimelineItem>
+     
+    </Timeline>
+  );
 }
 
 export default CustomTimeLine
